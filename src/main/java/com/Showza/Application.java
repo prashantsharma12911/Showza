07154d117f870d7f44593e891d0f44af23bffad4
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2026-09-19 00:16:34 +0530

    change from jar to war packaging

--- src/main/java/com/Showza/Application.java
+++ src/main/java/com/Showza/Application.java
@@ -2,11 +2,18 @@ package com.Showza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.Showza" })
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
